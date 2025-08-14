298793fe38f6e7ce129036b4d6ccd4654db78ee5
Shalaka Sonawane <shalaka@Shalakas-MacBook-Air.local>
2025-08-14 01:43:06 -0500

    preparing for deployment

---
+++ data/testimonial.js
@@ -0,0 +1,26 @@
+export const testimonial = [
+  {
+    quote:
+      "The AI-driven interview preparation completely transformed my approach—helped me secure my dream role at a leading tech company!",
+    author: "Diem Chang",
+    image: "https://randomuser.me/api/portraits/women/70.jpg",
+    role: "Software Engineer",
+    company: "Cots Tech Co.",
+  },
+  {
+    quote:
+      "The market insights were invaluable, enabling me to successfully shift my career direction. The salary predictions were incredibly accurate!",
+    author: "Roberto Monaco",
+    image: "https://randomuser.me/api/portraits/men/70.jpg",
+    role: "Product Manager",
+    company: "StartUp Inc.",
+  },
+  {
+    quote:
+      "Boosting my resume’s ATS score made all the difference—I received more interview calls in two weeks than I had in the previous six months!",
+    author: "Anna Smith",
+    image: "https://randomuser.me/api/portraits/women/61.jpg",
+    role: "Marketing Director",
+    company: "Global Corp",
+  },
+];
